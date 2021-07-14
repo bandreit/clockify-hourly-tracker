7e58206b5be9ae5a3664645088f82ba048eeb9ed
Andrei Bostan <dkAndBos@dkc8839.corp.lego.com>
2021-07-14 15:53:44 +0200

    first beta version working with warnings after generation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axiosInstance = require('axios')
-const { splitPeriodIntoWeeks } = require('./date-helper');
+const XlsxPopulate = require('xlsx-populate');
+const { splitPeriodIntoWeeks, getLastMonth15th, getThisMonth15th, getThisMonth16th, weekdays } = require('./date-helper');
 const key = process.env.CLOCKIFY_API_KEY || ''
 
 const axios = axiosInstance.create({
@@ -9,30 +10,24 @@ const axios = axiosInstance.create({
 
 const baseUrl = `https://api.clockify.me/api/v1`
 const reportsUrl = `https://reports.api.clockify.me/v1`
+let username = 'report'
 
 if (!key) {
     console.log(`API key must be provided through 'CLOCKIFY_API_KEY' env variable. Get one at https://clockify.me/user/settings`)
     process.exit(1)
 }
 
-let lastMonth15th = new Date();
-lastMonth15th.setHours(2, 0, 0, 0);
-lastMonth15th.setDate(0);
-lastMonth15th.setDate(16);
-
-let thisMonth15th = new Date();
-thisMonth15th.setHours(25, 59, 59, 0);
-thisMonth15th.setDate(15);
-
-let thisMonth16th = thisMonth15th;
-thisMonth16th.setDate(16);
+thisMonth15th = getThisMonth15th();
+thisMonth16th = getThisMonth16th();
+lastMonth15th = getLastMonth15th();
 
 ; (async () => {
     try {
         const baseUserResponse = await axios.get(`${baseUrl}/user`)
 
+        username = baseUserResponse.data.name;
         const workspaceId = baseUserResponse.data.defaultWorkspace
-        console.log(`Welcome, ${baseUserResponse.data.name}, this is your workspace: ${workspaceId}`)
+        console.log(`Welcome, ${username}, this is your workspace: ${workspaceId}`)
 
         const weeks = splitPeriodIntoWeeks(lastMonth15th, thisMonth15th);
         let promises = [];
@@ -61,8 +56,27 @@ thisMonth16th.setDate(16);
             })
         })
         weeklyTotals = weeklyTotals.filter(log => log.jsDate <= thisMonth16th && log.amount > 0)
-        console.log(weeklyTotals);
+        populateXlsFile(weeklyTotals);
     } catch (error) {
-        console.log(error.response.data.code + ': ' + error.response.data.message);
+        console.log(error);
+        // console.log(error.response.data.code + ': ' + error.response.data.message);
     }
 })()
+
+const populateXlsFile = (weeklyTotals) => {
+    XlsxPopulate.fromFileAsync("./template.xlsx")
+        .then(workbook => {
+            const r = workbook.sheet('Template').range(`B4:E${weeklyTotals.length}`);
+
+            // Set the values using a 2D array:
+            weeklyTotals = weeklyTotals.map(entry => [weekdays[entry.jsDate.getDay()], entry.date, ' ', entry.duration / 3600])
+
+            r.value(weeklyTotals);
+
+            workbook.sheet("Template").cell("C34").value(username);
+            workbook.sheet("Template").cell("C37").value((new Date()).toLocaleDateString('en-US'));
+            // Write to file.
+            console.log('Done.');
+            return workbook.toFileAsync(`./${username}.xlsx`);  
+        });
+}
